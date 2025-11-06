19db5d2d4c32425b15fbf6498ecaf3a91e94f128
Bommena Sai Chandra Vardhan Rao <saichandra@Bommenas-MacBook-Air.local>
2025-11-06 21:15:13 +0530

    add transform and load tasks

--- dags/weatherETL.py
+++ dags/weatherETL.py
@@ -36,3 +36,79 @@ with DAG(dag_id = 'weather_etl_pipeline',
         # https://api.open-meteo.com/v1/forecast?latitude=17.384&longitude=78.456&current_weather=true
         endpoint = f'/v1/forecast?latitude={LATITUDE}&longitude={LONGITUDE}&current_weather=true'
 
+        response = http_hook(endpoint)
+
+        if response.status_code == 200:
+            return response.json()
+        else:
+            raise Exception(f"Failed to fetch weather daya: {response.status_code}")
+        
+
+
+    # creating task to transform weather data
+    @task()
+    def transform_weather_data(weather_data):
+
+        current_weather = weather_data['current_weather']
+        transformed_data = {
+            'latitude' : LATITUDE,
+            'longitude' : LONGITUDE,
+            'temperature' : current_weather['temperature'],
+            'windspeed' : current_weather['windspeed'],
+            'winddirection' : current_weather['winddirection'],
+            'weathercode' : current_weather['weathercode']
+        }
+
+
+        return transformed_data
+
+
+    # task to load data into database
+    @task()
+    def load_weather_data(transformed_data):
+        pg_hook = PostgresHook(postgres_conn_id = POSTGRES_CONN_ID)
+        conn = pg_hook.get_conn()
+        cursor = conn.cursor()
+
+        cursor.execute(""" 
+        CREATE TABLE IF NOT EXISTS weather_data (
+                       latitude FLOAT,
+                       longitude FLOAT,
+                       temperature FLLAT,
+                       windspeed FLOAT,
+                       winddirection FLOAT,
+                       weathercode INT,
+                       timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+                       );
+                       """)
+        
+        cursor.execute("""
+                       INSERT INTO weather_data (latitude, longitude, temperature, windspeed, winddirection, weathercode)
+                       VALUES (%s, %s, %s, %s, %s)
+                       """, (
+                            transformed_data['latitude'],
+                            transformed_data['longitude'],
+                            transformed_data['temperature'],
+                            transformed_data['windspeed'],
+                            transformed_data['winddirection'],
+                            transformed_data['weathercode']
+                       ))
+        
+        conn.commit()
+        cursor.close()
+
+    # workflow of tasks (ETL Pipeline)
+    weather_data = extract_weather_data()
+    transformed_data = transform_weather_data(weather_data)
+    load_weather_data(transformed_data)
+
+
+    
+        
+
+
+        
+
+
+
+
